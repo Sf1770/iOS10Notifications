2aecc3acb2f8c2a24b726d5f367cafae0c539699
Sf1770 <sf1770@nyu.edu>
2017-11-08 17:42:20 -0500

    Initial Commit

---
+++ Notifications/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notifications
+//
+//  Created by Sabrina Fletcher on 11/8/17.
+//  Copyright © 2017 Sabrina Fletcher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
